271cead5433d86b1f5c3345f01c6647591823407
HongkyuKim <mark.khk@gmail.com>
2021-04-23 21:55:17 +0900

    보석쇼핑 풀이/테스트 케이스/README 추가

---
+++ kakao/JewelryShopping/REAME.md
@@ -0,0 +1,3 @@
+## 보석 쇼핑
+[문제링크]  
+https://programmers.co.kr/learn/courses/30/lessons/67258
---
+++ kakao/JewelryShopping/gems.py
@@ -0,0 +1,9 @@
+class Gems:
+    def __init__(self, result, gems):
+        self.gems = gems
+        self.result = result
+
+gems1 = Gems([3, 7], ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"])
+gems2 = Gems([1, 3], ["AA", "AB", "AC", "AA", "AC"])
+gems3 = Gems([1, 1], ["XYZ", "XYZ", "XYZ"])
+gems4 = Gems([1, 5], ["ZZZ", "YYY", "NNNN", "YYY", "BBB"])
